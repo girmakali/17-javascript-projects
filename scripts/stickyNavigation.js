075f99b54bee11d848f2b1df6a8725f8cf9a8121
girmakali <mogesgirma44@yahoo.com>
2024-01-01 10:39:48 -0800

    stickyNav

---
+++ scripts/stickyNavigation.js
@@ -0,0 +1,13 @@
+const navElement = document.querySelector('nav')
+const navBarLinks = document.querySelectorAll('a')
+
+const navPosition = navElement.getBoundingClientRect().top
+
+window.addEventListener('scroll', ()=>{
+  const scrollPosition = window.scrollY;
+  navElement.style.top = scrollPosition + 'px'
+
+  navBarLinks.forEach(link =>{
+    
+  })
+})
