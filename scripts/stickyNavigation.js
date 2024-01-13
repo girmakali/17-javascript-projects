3c16e59248a4b940fbccc620ad7923e491011acf
girmakali <mogesgirma44@yahoo.com>
2024-01-13 02:10:30 -0800

    stickeyNavigation

--- scripts/stickyNavigation.js
+++ scripts/stickyNavigation.js
@@ -1,13 +1,22 @@
 const navElement = document.querySelector('nav')
 const navBarLinks = document.querySelectorAll('a')
 
-const navPosition = navElement.getBoundingClientRect().top
+
 
 window.addEventListener('scroll', ()=>{
   const scrollPosition = window.scrollY;
   navElement.style.top = scrollPosition + 'px'
 
-  
-
+  navBarLinks.forEach((link,i) =>{
+    const sectionElement =document.getElementById(`section${i}`)
+    console.log(i)
+    
+    if(scrollPosition >= sectionElement.offsetTop && scrollPosition <  sectionElement.offsetTop + sectionElement.offsetHeight){
+      link.classList.add('active')
+    }
+    else{
+      link.classList.remove('active')
+    }
+})
   
 })
